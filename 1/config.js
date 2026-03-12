7d5cd41ac2395198069cda6a5cdd2cca2b6cbda8
suryamuralidhar <suryamurali006@gmail.com>
2026-03-12 16:58:31 +0530

    Add files via upload

---
+++ 1/config.js
@@ -0,0 +1,52 @@
+const CONFIG = {
+
+
+// ============================
+// PROJECT INFO
+// ============================
+
+title: "Villa Interior – Living Room",
+
+heading: "Living Room – Villa Interior",
+
+panoramaImage: "LIV2.png",
+
+
+// ============================
+// POPUP SETTINGS
+// ============================
+
+popupText: `
+<b>Disclaimer</b><br><br>
+If you are viewing this on a mobile device,
+please rotate your phone to <b>landscape</b>
+and use <b>fullscreen</b> for best experience.
+`,
+
+popupButton: "OK 😃",
+
+popupDelay: 1000,
+
+mobileWidth: 900,
+
+
+// ============================
+// VIEWER SETTINGS
+// ============================
+
+dragFriction: 0.5,
+
+
+// ============================
+// COLORS
+// ============================
+
+spaceBarColor: "rgba(0,0,0,0.5)",
+
+popupBackground: "white",
+
+popupOverlay: "rgba(0,0,0,0.55)",
+
+buttonColor: "#2e6f45"
+
+};
