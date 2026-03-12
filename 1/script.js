de0094b9c7997089835846d2a56322c32922054b
suryamuralidhar <suryamurali006@gmail.com>
2026-03-12 20:56:24 +0530

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -9,25 +9,59 @@ panorama: CONFIG.panoramaImage,
 autoLoad: true,
 friction: CONFIG.dragFriction
 
-});
 
 
-/* ===== BACKGROUND MUSIC ===== */
+
+
+
+
+
+  /* ===== BACKGROUND MUSIC DEBUG ===== */
+
+const debugBox = document.getElementById("musicDebug");
 
 const music = document.getElementById("bgMusic");
 
 if(music){
 
+debugBox.innerText = "Music Debug: Loading file...";
+
 music.src = CONFIG.musicFile;
 
 music.volume = CONFIG.musicVolume;
 
-music.play().catch(()=>{
+music.addEventListener("loadeddata", ()=>{
+debugBox.innerText = "Music Debug: File Loaded ✅";
+});
 
-console.log("Autoplay blocked until user interaction");
+music.addEventListener("playing", ()=>{
+debugBox.innerText = "Music Debug: Playing ▶️";
+});
+
+music.addEventListener("pause", ()=>{
+debugBox.innerText = "Music Debug: Paused ⏸️";
+});
+
+music.addEventListener("error", ()=>{
+debugBox.innerText = "Music Debug: Error loading music ❌";
+});
+
+music.play().then(()=>{
+
+debugBox.innerText = "Music Debug: Autoplay started ▶️";
+
+}).catch(()=>{
+
+debugBox.innerText = "Music Debug: Autoplay blocked ⚠️ Tap screen";
 
 });
 
 }
+else{
 
+debugBox.innerText = "Music Debug: Audio element NOT FOUND ❌";
+
+}
 });
+
+
