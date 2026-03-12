e500cc7c373cfcdeb17976021e26bcd57818ab60
suryamuralidhar <suryamurali006@gmail.com>
2026-03-12 21:18:15 +0530

    Update config.js

--- 1/config.js
+++ 1/config.js
@@ -4,20 +4,7 @@ title: "Villa Interior – Living Room",
 
 heading: "Living Room – Villa Interior",
 
-panoramaImage: "images/1.png",
-
-popupText: `
-<b>Disclaimer</b><br><br>
-You are viewing this on a mobile device,
-please rotate your phone to <b>Landscape</b>
-and use <b>Fullscreen</b> for the best experience.
-`,
-
-popupButton: "OK 😃",
-
-mobileWidth: 900,
-
-popupDelay: 1000,
+panoramaImage: "1.png",
 
 dragFriction: 0.5,
 
